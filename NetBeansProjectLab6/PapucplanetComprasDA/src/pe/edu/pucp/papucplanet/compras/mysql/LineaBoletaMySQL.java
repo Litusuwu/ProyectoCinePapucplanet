bc15788c2ddbc05ffc097b29e95421c091ed2b62
bry4nV <bryan31vj@gmail.com>
2024-09-25 02:24:26 -0500

    Version de Boleta/lineaBoleta

--- NetBeansProjectLab6/PapucplanetComprasDA/src/pe/edu/pucp/papucplanet/compras/mysql/LineaBoletaMySQL.java
+++ NetBeansProjectLab6/PapucplanetComprasDA/src/pe/edu/pucp/papucplanet/compras/mysql/LineaBoletaMySQL.java
@@ -5,17 +5,26 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import pe.edu.pucp.papucplanet.cine.dao.ButacaDAO;
 import pe.edu.pucp.papucplanet.cine.dao.ButacaFuncionDAO;
 import pe.edu.pucp.papucplanet.cine.dao.SedeDAO;
+import pe.edu.pucp.papucplanet.cine.model.Butaca;
 import pe.edu.pucp.papucplanet.cine.model.ButacaFuncion;
 import pe.edu.pucp.papucplanet.cine.model.Sala;
+import pe.edu.pucp.papucplanet.cine.mysql.ButacaFuncionMySQL;
+import pe.edu.pucp.papucplanet.cine.mysql.ButacaMySQL;
 import pe.edu.pucp.papucplanet.cine.mysql.SedeMySQL;
 import pe.edu.pucp.papucplanet.compras.dao.BoletaDAO;
 import pe.edu.pucp.papucplanet.compras.dao.LineaBoletaDAO;
+import pe.edu.pucp.papucplanet.compras.model.Boleta;
 import pe.edu.pucp.papucplanet.compras.model.LineaBoleta;
+import pe.edu.pucp.papucplanet.confiteria.dao.AlimentoDAO;
+import pe.edu.pucp.papucplanet.confiteria.dao.BebidaDAO;
 import pe.edu.pucp.papucplanet.confiteria.model.Alimento;
 import pe.edu.pucp.papucplanet.confiteria.model.Bebida;
 import pe.edu.pucp.papucplanet.confiteria.model.Consumible;
+import pe.edu.pucp.papucplanet.confiteria.mysql.AlimentoMySQL;
+import pe.edu.pucp.papucplanet.confiteria.mysql.BebidaMySQL;
 import pe.edu.pucp.papucplanet.dbmanager.model.DBManager;
 
 public class LineaBoletaMySQL implements LineaBoletaDAO{
@@ -23,160 +32,193 @@ public class LineaBoletaMySQL implements LineaBoletaDAO{
     private Connection con;
     private CallableStatement cs;
     
-//    @Override
-//    public int insertar(LineaBoleta lineaBoleta) {
-//        int resultado = 0;
-//        try{
-//            // Preparamos la llamada al procedimiento
-//            String sql = "{CALL INSERTAR_LINEA_BOLETA(?, ?, ?, ?, ?)}";
-//            cs = con.prepareCall(sql);
-//
-//            // Establecer los parámetros de entrada
-//            cs.registerOutParameter(1, java.sql.Types.INTEGER); // El parámetro de salida _id_lineaBoleta
-//            cs.setInt(2, lineaBoleta.getBoleta().getIdBoleta());
-//            if(lineaBoleta.getConsumible() == null){
-//                cs.setInt(3,0);
-//            }else{
-//                cs.setInt(3, lineaBoleta.getConsumible().getId());
-//            }
-//            if(lineaBoleta.getButacaFuncion() == null){
-//                cs.setInt(4, 0);
-//            }else{
-//                cs.setInt(4, lineaBoleta.getButacaFuncion().getIdButacaFuncion());
-//            }
-//            cs.setInt(5, lineaBoleta.getCantidad()); 
-//            // Ejecutar el procedimiento
-//            cs.executeUpdate();
-//
-//            // Obtener el ID del administrador generado (parámetro OUT)
-//            int idAdministrador = cs.getInt(1);
-//            lineaBoleta.setIdLineaBoleta(idAdministrador); // Establecer el ID en el objeto Administrador
-//            resultado = lineaBoleta.getIdLineaBoleta();
-//        }catch(SQLException ex){
-//            System.out.println(ex.getMessage());
-//        }finally{
-//            try{
-//                con.close();
-//            }
-//            catch(SQLException ex){
-//                System.out.println(ex.getMessage());
-//            }
-//        }
-//        return resultado;        
-//    }
-//
-//    @Override
-//    public int actualizar(LineaBoleta lineaBoleta) {
-//        int resultado = 0;
-//        try{
-//             con = DBManager.getInstance().getConnection();
-//
-//            // Preparamos la llamada al procedimiento
-//            String sql = "{CALL MODIFICAR_LINEA_BOLETA(?,?,?,?,?)}";
-//            cs = con.prepareCall(sql);
-//
-//            // Establecer los parámetros de entrada
-//            cs.setInt(1,lineaBoleta.getIdLineaBoleta());
-//            cs.setInt(2, lineaBoleta.getBoleta().getIdBoleta());
-//            cs.setInt(3, lineaBoleta.getConsumible().getId());
-//            cs.setInt(4, lineaBoleta.getButacaFuncion().getIdButacaFuncion());
-//            cs.setInt(5, lineaBoleta.getCantidad()); 
-//            // Ejecutar el procedimiento
-//            cs.executeUpdate();
-//            resultado = lineaBoleta.getIdLineaBoleta();
-//        }catch(SQLException ex){
-//            System.out.println(ex.getMessage());
-//        }finally{
-//            try{
-//                con.close();
-//            }
-//            catch(Exception ex){
-//                System.out.println(ex.getMessage());
-//            }
-//        }
-//        return resultado;               
-//    }
-//
-//    @Override
-//    public int eliminar(int id) {
-//        int resultado = 0;
-//        try{
-//            con = DBManager.getInstance().getConnection();
-//            cs = con.prepareCall("{call ELIMINAR_LINEA_BOLETA_X_ID(?)}");
-//                cs.setInt("_id_linea_boleta", id);
-//            resultado = cs.executeUpdate();
-//        }catch(SQLException ex){
-//            System.out.println(ex.getMessage());
-//        }finally{
-//            try{con.close();}catch(SQLException ex){System.out.println(ex.getMessage());}
-//        }
-//        return resultado;
-//    }
-//
-//    @Override
-//    public LineaBoleta  obtenerPorId(int id) {
-//        LineaBoleta lineaBoleta = new LineaBoleta();
-//        try{
-//            con = DBManager.getInstance().getConnection();
-//            cs = con.prepareCall("{call LISTAR_LINEA_BOLETA_X_ID(?)}");
-//            cs.setInt("_id_linea_boleta", id);
-//            rs = cs.executeQuery();
-//            int idBoleta = 0, idButacaFuncion = 0, idConsumible = 0;
-//            if(rs.next()){
-//               lineaBoleta.setIdLineaBoleta(rs.getInt("id_linea_boleta"));
-//               idBoleta = rs.getInt("fid_boleta");
-//               lineaBoleta.setBoleta(BoletaDAO.obtenerPorId(idBoleta));//falta implementar
-//               
-//               if (rs.next()) {
-//                   Consumible consumible;
-//                    // Asignar los valores desde el ResultSet
-//                    if(rs.getInt("id_bebida") != 0){
-//                        consumible = new Bebida();
-//                    }
-//                    else if(rs.getInt("id_alimento") != 0){
-//                        consumible = new Alimento();
-//                    }
-//                    else{
-//                        throw new IllegalArgumentException("No existe ese usuario.");
-//                    }
-//
-//                    consumible.setId(rs.getInt("id_consumible"));
-//                    lineaBoleta.setConsumible(consumible);
-//                }
-//               
-//               
-//               idButacaFuncion = rs.getInt("fid_butaca_funcion");
-//               lineaBoleta.setButacaFuncion(ButacaFuncionDAO.obtenerPorId(idButacaFuncion));//falta implementar
-//               lineaBoleta.setCantidad(rs.getInt("cantidad"));
-//            }
-//        }catch(SQLException ex){
-//            System.out.println(ex.getMessage());
-//        }finally{
-//            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
-//        }
-//        return lineaBoleta;        
-//    }
+    @Override
+    public int insertar(LineaBoleta lineaBoleta) {
+        int resultado = 0;
+        try{
+            // Preparamos la llamada al procedimiento
+            String sql = "{CALL INSERTAR_LINEA_BOLETA(?, ?, ?, ?, ?)}";
+            cs = con.prepareCall(sql);
+
+            // Establecer los parámetros de entrada
+            cs.registerOutParameter(1, java.sql.Types.INTEGER); // El parámetro de salida _id_lineaBoleta
+            cs.setInt(2, lineaBoleta.getBoleta().getIdBoleta());
+            if(lineaBoleta.getConsumible() == null){
+                cs.setInt(3,0);
+            }else{
+                cs.setInt(3, lineaBoleta.getConsumible().getId());
+            }
+            if(lineaBoleta.getButacaFuncion() == null){
+                cs.setInt(4, 0);
+            }else{
+                cs.setInt(4, lineaBoleta.getButacaFuncion().getIdButacaFuncion());
+            }
+            cs.setInt(5, lineaBoleta.getCantidad()); 
+            // Ejecutar el procedimiento
+            cs.executeUpdate();
+
+            // Obtener el ID del administrador generado (parámetro OUT)
+            int idAdministrador = cs.getInt(1);
+            lineaBoleta.setIdLineaBoleta(idAdministrador); // Establecer el ID en el objeto Administrador
+            resultado = lineaBoleta.getIdLineaBoleta();
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{
+                con.close();
+            }
+            catch(SQLException ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+        return resultado;        
+    }
+
+    @Override
+    public int actualizar(LineaBoleta lineaBoleta) {
+        int resultado = 0;
+        try{
+             con = DBManager.getInstance().getConnection();
+
+            // Preparamos la llamada al procedimiento
+            String sql = "{CALL MODIFICAR_LINEA_BOLETA(?,?,?,?,?)}";
+            cs = con.prepareCall(sql);
+
+            // Establecer los parámetros de entrada
+            cs.setInt(1,lineaBoleta.getIdLineaBoleta());
+            cs.setInt(2, lineaBoleta.getBoleta().getIdBoleta());
+            cs.setInt(3, lineaBoleta.getConsumible().getId());
+            cs.setInt(4, lineaBoleta.getButacaFuncion().getIdButacaFuncion());
+            cs.setInt(5, lineaBoleta.getCantidad()); 
+            // Ejecutar el procedimiento
+            cs.executeUpdate();
+            resultado = lineaBoleta.getIdLineaBoleta();
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{
+                con.close();
+            }
+            catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+        return resultado;               
+    }
+
+    @Override
+    public int eliminar(int id) {
+        int resultado = 0;
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call ELIMINAR_LINEA_BOLETA_X_ID(?)}");
+                cs.setInt("_id_linea_boleta", id);
+            resultado = cs.executeUpdate();
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(SQLException ex){System.out.println(ex.getMessage());}
+        }
+        return resultado;
+    }
+
+    @Override
+    public LineaBoleta  obtenerPorId(int id) {
+        LineaBoleta lineaBoleta = new LineaBoleta();
+        BoletaDAO boleta = new BoletaMySQL();
+        ButacaFuncionDAO butacaFuncion = new ButacaFuncionMySQL();
+        
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call LISTAR_LINEA_BOLETA_X_ID(?)}");
+            cs.setInt("_id_linea_boleta", id);
+            rs = cs.executeQuery();
+            int idBoleta = 0, idButacaFuncion = 0, idConsumible = 0;
+            if(rs.next()){
+               lineaBoleta.setIdLineaBoleta(rs.getInt("id_linea_boleta"));
+               idBoleta = rs.getInt("fid_boleta");
+               lineaBoleta.setBoleta(boleta.obtenerPorId(idBoleta));//falta implementar
+               
+               if (rs.next()) {
+                   Consumible consumible;
+                    // Asignar los valores desde el ResultSet
+                    if(rs.getInt("id_bebida") != 0){
+                        consumible = new Bebida();
+                    }
+                    else if(rs.getInt("id_alimento") != 0){
+                        consumible = new Alimento();
+                    }
+                    else{
+                        throw new IllegalArgumentException("No existe ese usuario.");
+                    }
+
+                    consumible.setId(rs.getInt("id_consumible"));
+                    lineaBoleta.setConsumible(consumible);
+                }
+               
+               
+               idButacaFuncion = rs.getInt("fid_butaca_funcion");
+               lineaBoleta.setButacaFuncion(butacaFuncion.obtenerPorId(idButacaFuncion));//falta implementar
+               lineaBoleta.setCantidad(rs.getInt("cantidad"));
+            }
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return lineaBoleta;        
+    }
 
     @Override
     public ArrayList<LineaBoleta> listar() {
-        ArrayList<LineaBoleta> lineaBoletas = new ArrayList<>();
+        ArrayList<LineaBoleta> lineasBoletas = new ArrayList<>();
+        int idBoleta,idConsumible,idButaca;
+        BoletaDAO boletaSQL = new BoletaMySQL();
+        ButacaFuncionDAO butacaFuncionSQL = new ButacaFuncionMySQL();
+        AlimentoDAO alimentoSQL = new AlimentoMySQL();
+        Alimento alimento;
+        BebidaDAO bebidaSQL = new BebidaMySQL();
+        Bebida bebida;
+        Boleta boleta;
+        ButacaFuncion butacaFuncion;
+                
         try{
             con = DBManager.getInstance().getConnection();
             con.setAutoCommit(false);
-            cs = con.prepareCall("{call LISTAR_LINEAS_BOLETAS_TODAS()}");
+            
+            cs = con.prepareCall("{CALL INFORMACION_EXTRA_LINEA_BOLETA()}");
             rs = cs.executeQuery();
-            LineaBoleta lineaboleta;
-            int idSede,idBoleta,idConsumible,idButaca;
-            SedeDAO sedeDao = new SedeMySQL();
+            
             while(rs.next()){
-                lineaboleta = new LineaBoleta();
-                lineaboleta.setIdLineaBoleta(rs.getInt("id_linea_boleta"));
+                
+                LineaBoleta lineaboleta = new LineaBoleta();
+                // Asignar los valores desde el ResultSet  
+                idConsumible = rs.getInt("id_consumible");
+                
+                if(rs.getInt("id_alimento") != 0){
+                    alimento = alimentoSQL.obtenerPorId(idConsumible);
+                    lineaboleta.setConsumible(alimento);
+                }
+                else if(rs.getInt("id_bebida") != 0){
+                    bebida = bebidaSQL.obtenerPorId(idConsumible);
+                    lineaboleta.setConsumible(bebida);
+                }
+                else{
+                    throw new IllegalArgumentException("No existe ese usuario.");
+                }
+               
                 idBoleta = rs.getInt("fid_boleta");
-                lineaBoleta.setBoleta(BoletaDAO.obtenerPorId(idBoleta));//falta implementar
-                idSede = rs.getInt("fid_sede");
-                lineaboleta.setSede(sedeDao.obtenerPorId(idSede));
-                lineaboleta.setCapacidad(rs.getInt("capacidad"));
-                salas.add(lineaboleta);
+                idButaca = rs.getInt("fid_butaca");
+                
+                boleta = boletaSQL.obtenerPorId(idBoleta);
+                butacaFuncion = butacaFuncionSQL.obtenerPorId(idButaca);
+                
+                lineaboleta.setBoleta(boleta);
+                lineaboleta.setButacaFuncion(butacaFuncion);
+                lineaboleta.setCantidad(rs.getInt("cantidad"));
+                lineaboleta.setActivo(rs.getBoolean("activo"));
+                lineaboleta.setIdLineaBoleta(rs.getInt("id_linea_boleta"));
+                lineasBoletas.add(lineaboleta);
             }
             con.commit();
         }catch(SQLException ex){
@@ -184,6 +226,6 @@ public class LineaBoletaMySQL implements LineaBoletaDAO{
         }finally{
             try{con.close();}catch(SQLException ex){System.out.println(ex.getMessage());}
         }
-        return lineaBoletas;
+        return lineasBoletas;
     }
 }
