b434cfdab5ede58b4734e9a8877b5f59dad549cc
SereSoyer <derm20102010@hotmail.com>
2024-09-25 01:01:28 -0500

    Boletaaas

--- NetBeansProjectLab6/PapucplanetGestionUsuarioModel/src/pe/edu/pucp/papucplanet/gestionUsuario/model/Cliente.java
+++ NetBeansProjectLab6/PapucplanetGestionUsuarioModel/src/pe/edu/pucp/papucplanet/gestionUsuario/model/Cliente.java
@@ -2,11 +2,8 @@ package pe.edu.pucp.papucplanet.gestionUsuario.model;
 import java.util.ArrayList;
 import java.util.Date;
 import pe.edu.pucp.papucplanet.cine.model.Sede;
-import pe.edu.pucp.papucplanet.compras.model.Boleta;
 public class Cliente extends Usuario{	
     private Sede sede;
-    //posee un arrayList de boletas
-    private ArrayList<Boleta> boletas;
     // Constructor con parámetros para Cliente y Usuario
     public Cliente(){
         
@@ -30,25 +27,12 @@ public class Cliente extends Usuario{
         this.sede = sede;
     }
 
-    public ArrayList<Boleta> getBoletas(){
-        return new ArrayList<>(boletas);
-    }
-
-    public void setBoletas(ArrayList<Boleta> boletas){
-        this.boletas = boletas;
-    }
     // Método para generar un reporte de todas las boletas del cliente
     @Override
     public String emitirReporte() {
         String reporte = "";
         reporte = "Reporte de boletas para el cliente: " + getNombre() + "\n";
         reporte = "Universidad: " + sede.getUniversidad() + "\n";
-        reporte = "Boletas: \n";
-
-        for (Boleta boleta : boletas) {
-            reporte = " - Fecha de compra: " + boleta.getFechaCompra() +", Total: " + boleta.getTotal()+ "\n";
-        }
-
         return reporte;
     }
     
