061b38c312efd91710ff1bb31b2c2344fb1efb20
claraaqwq <a20210944@pucp.edu.pe>
2024-10-29 20:29:20 -0500

    FuncionMetodos

--- NetBeansProjects/PapucplanetWSBL/src/java/pe/edu/pucp/papucplanet/cine/services/FuncionWS.java
+++ NetBeansProjects/PapucplanetWSBL/src/java/pe/edu/pucp/papucplanet/cine/services/FuncionWS.java
@@ -24,6 +24,66 @@ public class FuncionWS {
         }
         return funciones;
     }
-   
+    
+    @WebMethod(operationName = "insertarFuncion")
+    public int insertar(@WebParam(name = "funcion") Funcion funcion) {
+        int resultado = 0;
+        try {
+            daoFuncion = new FuncionMySQL();
+            resultado = daoFuncion.insertar(funcion);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return resultado;
+    }
+    
+    @WebMethod(operationName = "modificarFuncion")
+    public int modificar(@WebParam(name = "funcion") Funcion funcion) {
+        int resultado = 0;
+        try {
+            daoFuncion = new FuncionMySQL();
+            resultado = daoFuncion.modificar(funcion);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return resultado;
+    }
+    
+    @WebMethod(operationName = "eliminarFuncion")
+    public int eliminar(@WebParam(name = "idFuncion") int idFuncion) {
+        int resultado = 0;
+        try {
+            daoFuncion = new FuncionMySQL();
+            resultado = daoFuncion.eliminar(idFuncion);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return resultado;
+    }
+    
+    @WebMethod(operationName = "obtenerPorIdFuncion")
+    public Funcion obtenerPorId(@WebParam(name = "idFuncion") int idFuncion) {
+        Funcion funcion = null;
+        try {
+            daoFuncion = new FuncionMySQL();
+            funcion = daoFuncion.obtenerPorId(idFuncion);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return funcion;
+    }
+    
+    @WebMethod(operationName = "obtenerFuncionesPorPeliculaFuncion")
+    public ArrayList<Funcion> obtenerFuncionesPorPeliculaFuncion(@WebParam(name = "idPelicula") int idPelicula) {
+        ArrayList<Funcion> funciones = null;
+        try {
+            daoFuncion = new FuncionMySQL();
+            funciones = daoFuncion.obtenerFuncionesPorPelicula(idPelicula);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return funciones;
+    }
+    
     
 }
